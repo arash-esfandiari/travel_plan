35779ee2ba27bf33db07c8a9b365e64bb71fb67e
Arash Esfandiari <arashesfandiari@yahoo.com>
2025-04-12 04:21:34 +0200

    logo chagned, header updated, input validation added

--- src/components/Shared/Footer.js
+++ src/components/Shared/Footer.js
@@ -4,7 +4,14 @@ import './Footer.css';
 const Footer = () => {
     return (
         <footer>
-            <p>&copy; {new Date().getFullYear()} Travel Planner. All rights reserved.</p>
+            <div className="footer-content">
+                <p>&copy; {new Date().getFullYear()} Smart Travel Planner by Esfandiari Inc. All rights reserved.</p>
+                <div className="footer-links">
+                    <a href="/privacy-policy">Privacy Policy</a>
+                    <a href="/terms-of-service">Terms of Service</a>
+                    <a href="/contact">Contact Us</a>
+                </div>
+            </div>
         </footer>
     );
 };
--- src/components/Shared/Header.js
+++ src/components/Shared/Header.js
@@ -1,20 +1,24 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../context/AuthContext';
 import './Header.css';
-import logo from '../../assets/images/logos/logo_horz.png';
+import logo from '../../assets/images/logos/logo-horz.png';
 
 const Header = () => {
     const { user, logout } = useContext(AuthContext);
     const navigate = useNavigate();
+    const [isFading, setIsFading] = useState(false);
 
     const handleLogout = () => {
-        logout();
-        navigate('/');
+        setIsFading(true); // Trigger fade-out effect
+        setTimeout(() => {
+            logout();
+            navigate('/');
+        }, 1000); // Match the fade-out duration
     };
 
     return (
-        <header>
+        <header className={isFading ? 'fade-out' : ''}>
             <div className="logo">
                 <Link to="/">
                     <img src={logo} alt="Logo" />
